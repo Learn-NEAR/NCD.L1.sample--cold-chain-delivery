9204f82b60cb94263d7128efd74ac6d4ef84c4bb
Quan Tran <quantran90@hotmail.com>
2021-12-29 13:21:24 +0700

    fix: update build.sh to match the package's name - NCD...

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/ncd_playground.wasm ./res/
+cp target/wasm32-unknown-unknown/release/NCD_playground.wasm ./res/
 
